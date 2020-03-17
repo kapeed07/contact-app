d17852e9e93a5e77d69a83e1d547be20eebe0f47
Deepak Bhardwaj <deepak@customersuccessbox.com>
2020-03-17 23:29:10 +0530

    add deploy.sh | add basic project srtucture | add tailwind

---
+++ src/components/contacts/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Contacts</h1>
+    <list
+      v-for="(contact, index) in contacts"
+      :key="index"
+      :contact="contact"
+    ></list>
+  </div>
+</template>
+
+<script>
+import List from "./partials/list";
+
+export default {
+  name: "contact-index",
+  data() {
+    return {
+      contacts: [
+        {
+          first_name: "Deepak",
+          last_name: "Bhardwaj",
+          mobile: "9953575646"
+        },
+        {
+          first_name: "Kapeed",
+          last_name: "Bhardwaj",
+          mobile: "9999999999"
+        }
+      ]
+    };
+  },
+  components: {
+    List
+  }
+};
+</script>
+
+<style></style>
---
+++ src/components/contacts/partials/list.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{contact.first_name}} | {{contact.last_name}} | {{contact.mobile}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "contact-list",
+  props: ["contact"]
+}
+</script>
+
+<style>
+
+</style>
